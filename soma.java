b8be65790bb6ce241a90e785e0c1b29f2781a208
phrzdgal <phiroze.khattra@gmail.com>
2022-02-04 16:23:29 -0800

    created lib folder and failing test

--- soma.java
+++ soma.java
@@ -1,1 +1,5 @@
-public class soma {}
+public class soma {
+    public static int subtraction(int n1, int n2) {
+        return n1 - n2;
+    }
+}
---
+++ somatest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class somatest {
+    @Test
+    public void subtraction() {
+        assertEquals(2, soma.subtraction(5, 1)); 
+    }
+}
